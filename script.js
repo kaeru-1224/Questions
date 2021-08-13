d84d73be15ccb949eecfbd55397526f830f8434f
JISU <cjfslzpf@naver.com>
2021-08-13 15:11:01 +0900

    0813

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const btn1 = document.querySelectorAll(".btn");
-const a1= document.querySelectorAll(".answer-text")
+//1)버튼을 선택해서 각 버튼에 이벤트 리스너를 부여해주기 
 
-function showAnswer(){
-    a1.forEach(a1=>{if(a1[0]){console.log(a1[0])}} )
-
-    
-}
+//부모노드의 형제들에  hidden의유무로 지우고 넣기로 할 수있었음 
+//문제: 한개가 열릴시 다른 부분이 닫히는 것을 못함 
+const btns= document.querySelectorAll("button");
+const answer= document.querySelectorAll(".answer-text")
 
 
-btn1.forEach(btn=> {btn.addEventListener('click',showAnswer)})
+
+
+
+btns.forEach(openbtn=>{openbtn.addEventListener('click',function(e){
+    const questions= e.currentTarget.parentNode;
+  
+    const result= questions.nextElementSibling;
+    result.classList.toggle("hidden");})})
+
