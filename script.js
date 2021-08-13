badb8d9fb3868ccc2d6db1bb2fd5d5209a37ddb3
JISU <cjfslzpf@naver.com>
2021-08-13 17:05:04 +0900

    0813

--- script.js
+++ script.js
@@ -2,16 +2,43 @@
 
 //부모노드의 형제들에  hidden의유무로 지우고 넣기로 할 수있었음 
 //문제: 한개가 열릴시 다른 부분이 닫히는 것을 못함 
+/*
 const btns= document.querySelectorAll("button");
-const answer= document.querySelectorAll(".answer-text")
-
-
 
 
 
 btns.forEach(openbtn=>{openbtn.addEventListener('click',function(e){
+    const answer= document.querySelectorAll(".answer-text")
     const questions= e.currentTarget.parentNode;
-  
     const result= questions.nextElementSibling;
-    result.classList.toggle("hidden");})})
+    answer.forEach(
+        e=> {
+            const hidden =e.classList.contains("hidden");
+            if(hidden!==3){
+                result.classList.toggle("hidden")
+            }
+        }
+    )
+})})*/
+
+
+const questions= document.querySelectorAll(".questions-text")
+
+questions.forEach( question=> {
+    const btn =question.querySelector("button");
+    btn.addEventListener('click',()=>{
+        questions.forEach(item=> {
+           if(item===question){
+               item.querySelector(".answer-text").classList.remove("hidden")
+
+           }else{item.querySelector(".answer-text").classList.add("hidden")}
+        })
+
+
+    })
+        
+        });
+  
+        
+
 
